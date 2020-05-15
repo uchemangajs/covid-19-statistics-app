15760df68f4470b8ae7cf13c9cf1937322e5be78
uchennacn <uchennanwajideobi@gmail.com>
2020-05-15 22:45:33 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,208 @@
+'use strict';
+
+const $collapsible = document.getElementsByClassName('collapsible');
+const $dropdownMenu = document.querySelector('[dropdown-menu]');
+const $barBtn = document.querySelector('[bar-btn]');
+const $totalCases = document.querySelector('[total_cases]');
+const $totalRecovered = document.querySelector('[total_recovered]');
+const $totalDeaths = document.querySelector('[total_deaths]');
+let data = {};
+console.log($collapsible);
+
+    function toggleMenu () {
+        if($dropdownMenu.style.display === 'none'){
+           $dropdownMenu.style.display = '';
+            console.log('yes');
+            
+            
+        }else{
+            $dropdownMenu.style.display = 'none';
+    }}
+
+$barBtn.addEventListener("click", toggleMenu);
+
+
+for(let i=0; i < $collapsible.length; i++){
+   console.log($collapsible[i])
+    $collapsible[i].addEventListener("click", togglefxn);
+
+    const $chartDiv = $collapsible[i].querySelector('[chart-div]');
+
+    const $upArrow = $collapsible[i].querySelector('[up-arrow]');
+    const $downArrow = $collapsible[i].querySelector('[down-arrow]');
+
+    function togglefxn () {
+        if($chartDiv.style.display === 'none'){
+          $chartDiv.style.display = '';
+            
+        }else{
+        $chartDiv.style.display = 'none';
+    }
+    
+    
+        if($downArrow.style.display === ''){
+          $downArrow.style.display = 'none';
+          $upArrow.style.display = '';
+            
+        }else{
+        $upArrow.style.display = 'none';
+        $downArrow.style.display = '';
+    }
+    (function showChart () {
+        var ctx = document.getElementById('myChart1').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+    
+        // Configuration options go here
+        options: {}
+    });
+
+    var ctx = document.getElementById('myChart2').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+    
+        // Configuration options go here
+        options: {}
+    });
+
+    var ctx = document.getElementById('myChart3').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+    
+        // Configuration options go here
+        options: {}
+    });
+    })();
+}
+    }
+    
+    
+
+
+function logResult(result) {
+   
+   console.log(result);
+    data = result;
+    $totalCases.innerText = data.total_cases;
+    $totalRecovered.innerText  = data.total_recovered;
+    $totalDeaths.innerText = data.total_deaths
+  }
+  
+  function logError(error) {
+    console.log('Looks like there was a problem:', error);
+  }
+  
+  function validateResponse(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
+  
+  function readResponseAsJSON(response) {
+    return response.json();
+    
+  }
+  
+  function readResponseAsBlob(response) {
+    return response.blob();
+  }
+  
+  function readResponseAsText(response) {
+    return response.text();
+  }
+  
+  function showImage(responseAsBlob) {
+    const container = document.getElementById('img-container');
+    const imgElem = document.createElement('img');
+    container.appendChild(imgElem);
+    const imgUrl = URL.createObjectURL(responseAsBlob);
+    imgElem.src = imgUrl;
+  }
+  
+  function showText(responseAsText) {
+    const message = document.getElementById('message');
+    message.textContent = responseAsText;
+  }
+
+  (function fetchJSON() {
+    //   const apiLink = https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php;
+    //   const apiKey = 6195ba9f20mshde087fdcc4aa35dp124092jsna642f0179fa6;
+    //   const api = 'apiLink/apiKey'
+      fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+            "x-rapidapi-key": "6195ba9f20mshde087fdcc4aa35dp124092jsna642f0179fa6"
+        }
+    })
+      .then(validateResponse)
+      .then(readResponseAsJSON)
+      .then(logResult)
+      .catch(logError);
+  })();
+
+  (function showNews (){
+    fetch("https://newsapi.org/v2/top-headlines?q=covid-19&apiKey=91382907a2c04a9ba9e60ca8bbac7643").then(data => {
+        return data.json()
+    }).then(d => {
+        console.log(d);
+        // articles = d.articles;
+        Array.from(d.articles).forEach(news => {
+            document.querySelector("#news").innerHTML+=`
+            <a href="${news.url}">
+                <div class="article">
+                 <h3>${news.title}</h3>
+                 <div class="imgHolder">
+                     <img src="${news.urlToImage}"  alt="">
+                 </div>
+                 <p>${news.description} </p>
+                </div>
+                </a>`
+        })
+    });
+
+  })();
+  
+
+
+
+
+
+
