f4cfdf686104dd39dbfdf966fdd4e808da9494da
Uchenna <uchemanga.js@gmail.com>
2020-09-04 21:30:00 +0100

    PWA and Nav

--- js/nav.js
+++ js/nav.js
@@ -8,7 +8,12 @@ window.onscroll = function() {navStyle()};
 function navStyle() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.querySelector("[nav]").className = "nav-colored";
+      document.querySelector("[sbstyle-scroll]").className = "sbstyle-scroll";
+      document.querySelector("[topic-link]").className = "topic-link";
+      
   } else {
     document.querySelector("[nav]").className = "";
+      document.querySelector("[sbstyle-scroll]").className = "sbstyle";
+      document.querySelector("[topic-link]").className = "";
   }
 }
